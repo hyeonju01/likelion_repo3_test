312ec23e1450334970d94ce753a796f9ca9d5ff8
hyeonju01 <tangled6648@gmail.com>
2022-10-05 10:48:23 +0900

    Calculator와 CalculatorMain 파일 분리, 0.5가 나오도록 문제 해결 Calculator 생성자 작성 및 CalculatorMain 내 파라미터 없이 메소드만 사용!

--- src/week3/day2/Calculator.java
+++ src/week3/day2/Calculator.java
@@ -3,39 +3,30 @@ package week3.day2;
 import com.sun.security.jgss.GSSUtil;
 
 public class Calculator {
-    int a;
-    int b;
+    private int a;
+    private int b;
 
-    Calculator() {
-        this.a = 0;
-        this.b = 0;
-    }
-
-    public void plus(int a, int b) {
+    Calculator(int a, int b) {
         this.a = a;
         this.b = b;
-        System.out.println(a+b);
     }
 
-    public void minus(int a, int b) {
-        this.a = a;
-        this.b = b;
+    public void plus() {
+        System.out.println(a + b);
+    }
+
+    public void minus() {
         System.out.println(a - b);
     }
 
-    public void multiple(int a, int b){
-        this.a = a;
-        this.b = b;
+    public void multiple(){
         System.out.println(a * b);
     }
 
-    public void divide(int a, int b){
-        this.a = a;
-        this.b = b;
-        double result = (double)a/b;
+    public void divide(){
         if (b==0){
             System.out.println("0으로 나눌 수 없습니다.");
         }
-        System.out.println(result);
+        System.out.println((double)a/b);
     }
 }
--- src/week3/day2/CalculatorMain.java
+++ src/week3/day2/CalculatorMain.java
@@ -2,10 +2,10 @@ package week3.day2;
 
 public class CalculatorMain {
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
-        calc.plus(10, 20);
-        calc.minus(10, 20);
-        calc.multiple(10, 20);
-        calc.divide(10, 20);
+        Calculator calc = new Calculator(10, 20);
+        calc.plus();
+        calc.minus();
+        calc.multiple();
+        calc.divide();
     }
 }
