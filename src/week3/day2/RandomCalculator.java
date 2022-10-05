cc3771764545a195632efbadb2b065807275f16d
hyeonju01 <tangled6648@gmail.com>
2022-10-05 11:27:08 +0900

    RandomCalculator, RandomCalculatorMain 예제_print함수 및 띄어쓰기 수정 + 코드 디벨롭

--- src/week3/day2/RandomCalculator.java
+++ src/week3/day2/RandomCalculator.java
@@ -2,30 +2,37 @@ package week3.day2;
 
 public class RandomCalculator {
 
+    int num;
     int RandomNumber = (int)(Math.random() * 10) + 0;
 
-    public void getRandomNumber(){
-        System.out.println("랜덤 넘버는 " + RandomNumber + " 입니다.");
+    RandomCalculator(int a){
+        this.num = a;
     }
-    public void plus(int a){
-        System.out.println(RandomNumber);
-        System.out.println(a + RandomNumber);
+    public void getAllNumber(){
+        System.out.println("입력된 수는 " + num + " 입니다.");
+        System.out.println("랜덤 수는 " + RandomNumber + " 입니다.");
     }
-
-    public void minus(int a){
-        System.out.println(a - RandomNumber);
-    }
-
-    public void multiple(int a){
-        System.out.println(a * RandomNumber);
-
+    public void plus(){
+        int result = num + RandomNumber;
+        System.out.println("덧셈 결과는: " + result);
     }
 
-    public void divide(int a){
+    public void minus(){
+        int result = num - RandomNumber;
+        System.out.println("뺄셈 결과는: " + result);
+    }
+
+    public void multiple(){
+        int result = num + RandomNumber;
+        System.out.println("곱셈 결과는: " + result);
+    }
+
+    public void divide(){
         if (RandomNumber == 0){
             System.out.println("랜덤 넘버가 0입니다.");
         }else {
-            System.out.println(a / RandomNumber);
+            int result = num / RandomNumber;
+            System.out.println("나눗셈 결과는: " + result);
         }
     }
 }
--- src/week3/day2/RandomCalculatorMain.java
+++ src/week3/day2/RandomCalculatorMain.java
@@ -2,11 +2,11 @@ package week3.day2;
 
 public class RandomCalculatorMain {
     public static void main(String[] args) {
-        RandomCalculator RC = new RandomCalculator();
-        RC.getRandomNumber();
-        RC.plus(10);
-        RC.minus(10);
-        RC.multiple(10);
-        RC.divide(10);
+        RandomCalculator RC = new RandomCalculator(10);
+        RC.getAllNumber();
+        RC.plus();
+        RC.minus();
+        RC.multiple();
+        RC.divide();
     }
 }
