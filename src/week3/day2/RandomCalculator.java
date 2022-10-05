1c42c69f559eb336c1e229ab6ffb63edc02d0fac
hyeonju01 <tangled6648@gmail.com>
2022-10-05 11:20:09 +0900

    RandomCalculator, RandomCalculatorMain 예제

---
+++ src/week3/day2/RandomCalculator.java
@@ -0,0 +1,31 @@
+package week3.day2;
+
+public class RandomCalculator {
+
+    int RandomNumber = (int)(Math.random() * 10) + 0;
+
+    public void getRandomNumber(){
+        System.out.println("랜덤 넘버는: " + RandomNumber + " 입니다.");
+    }
+    public void plus(int a){
+        System.out.println(RandomNumber);
+        System.out.println(a + RandomNumber);
+    }
+
+    public void minus(int a){
+        System.out.println(a - RandomNumber);
+    }
+
+    public void multiple(int a){
+        System.out.println(a * RandomNumber);
+
+    }
+
+    public void divide(int a){
+        if (RandomNumber == 0){
+            System.out.println("랜덤 넘버가 0입니다.");
+        }else {
+            System.out.println(a / RandomNumber);
+        }
+    }
+}
---
+++ src/week3/day2/RandomCalculatorMain.java
@@ -0,0 +1,12 @@
+package week3.day2;
+
+public class RandomCalculatorMain {
+    public static void main(String[] args) {
+        RandomCalculator RC = new RandomCalculator();
+        RC.getRandomNumber();
+        RC.plus(10);
+        RC.minus(10);
+        RC.multiple(10);
+        RC.divide(10);
+    }
+}
