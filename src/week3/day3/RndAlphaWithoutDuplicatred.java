3965f82df12ee437db68ba4930eb96604c69f476
hyeonju01 <tangled6648@gmail.com>
2022-10-06 15:51:43 +0900

    221006 알파벳 생성기

--- src/week3/day3/RndAlphaWithoutDuplicatred.java
+++ src/week3/day3/RndAlphaWithoutDuplicatred.java
@@ -3,18 +3,24 @@ package week3.day3;
 import com.sun.security.jgss.GSSUtil;
 
 import java.util.HashSet;
-import java.util.Set;
 
 public class RndAlphaWithoutDuplicatred {
     public static void main(String[] args) {
-
+        //알파벳 생성기 클래스 객체 생성
         RndAlphaGenerator rndAlphaGenerator = new RndAlphaGenerator();
+
         HashSet<Character> rndAlpha = new HashSet<>();
 
-        for(int i = 0; i < 200; i++){
+        for (int i = 0; i < 200; i++) {
             char a = rndAlphaGenerator.generate();
             rndAlpha.add(a);
         }
+
+        //while문으로도 작성해볼 수 있다.
+        //while ( cnt<100) {
+        //rndAlpha.add(rndAlphaGenerator.generate());
+        //cnt ++;
+        //}
 
         System.out.println(rndAlpha);
     }
