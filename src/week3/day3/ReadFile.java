e17ce3c1a72fbf5ce42efeec47515667934d90bb
hyeonju01 <tangled6648@gmail.com>
2022-10-06 15:50:24 +0900

    221006 한 글자 읽기 실습

--- src/week3/day3/ReadFile.java
+++ src/week3/day3/ReadFile.java
@@ -3,25 +3,35 @@ package week3.day3;
 //파일 입출력 실습: 1byte씩 읽기
 
 import java.io.BufferedReader;
+패import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class ReadFile {
 
+
+
+    public void printDirFiles(String filepath){
+
+    }
+
     public void bufferedReader(String filename) throws IOException {
         BufferedReader br = new BufferedReader(
                 new FileReader(filename), 16 * 1024);
-        System.out.println((char)(br.read()));
+        //버퍼 사이즈 : 16킬로바이트
+        System.out.println((br.read()));
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        //FileNotFoundException 발생 가능하므로 throws 해준다.
         ReadFile readFile = new ReadFile();
         try{
             readFile.bufferedReader("./a_file.txt");
+
         }catch(IOException e){
             e.printStackTrace();
         }
 
-
+        readFile.printDirFiles("./");
     }
 }
