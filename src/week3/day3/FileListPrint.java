cc6561210234b93ffb4d51069e18cac707afd7ee
hyeonju01 <tangled6648@gmail.com>
2022-10-06 15:43:48 +0900

    221006 파일 입출력 실습, 파일의 맨 앞 두 글자 출력하기

--- src/week3/day3/FileListPrint.java
+++ src/week3/day3/FileListPrint.java
@@ -1,15 +1,35 @@
 package week3.day3;
 import java.io.*;
-
+//2글자 읽기
 public class FileListPrint {
 
-    public static void main(String[] args) {
-
+    public void printFiles() {
         File dir = new File("./");
         File files[] = dir.listFiles();
         for (File file : files) {
             System.out.println(file);
         }
+    }
+
+    public char readAChar() throws IOException {
+        FileReader fileReader = new FileReader("파일네임");
+        return (char)fileReader.read();
+    }
+
+    public String read2Chars(String filename) throws IOException {
+        FileReader fileReader = new FileReader(filename);
+        String str = "";
+        str = str + (char) fileReader.read();
+        str = str + (char) fileReader.read();
+        return str;
+    }
+    public static void main(String[] args) throws IOException{
+        FileListPrint filelistprint = new FileListPrint();
+        String str = filelistprint.read2Chars("./a_file.txt");
+//        System.out.println(fileReader.read()); //ascii코드 반환
+//        System.out.println((char)fileReader.read()); //문자 반환
+        System.out.println(str);
+
     }
 
 }
